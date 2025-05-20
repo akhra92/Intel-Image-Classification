5b3a88433dc306b8c41a7e5de30586c8c9e0ea22
akhra92 <akhrorrakhmonov@gmail.com>
2025-05-20 11:52:57 +0900

    Update dataloader.py file by adding get_loaders function

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,36 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from torchvision import transforms as T
+from torchvision import datasets
 import config as cfg
+from glob import glob
+
+
+def get_transforms(train=True):
+
+    if train:
+        return T.Compose([T.Resize((cfg.IMAGE_HEIGHT, cfg.IMAGE_WIDTH)),
+                          T.RandomAffine(degrees=0.3, translate=0.3, scale=0.3),
+                          T.ColorJitter(brightness=0.3, saturation=0.3, hue=0.3),
+                          T.ToTensor()])
+    else:
+        return T.Compose([T.Resize((cfg.IMAGE_HEIGHT, cfg.IMAGE_WIDTH)),
+                          T.ToTensor()])
+    
+
+def get_loaders(root, train=True):
+    
+    trn_transforms = get_transforms(train=True)
+    val_transforms = get_transforms(train=False)
+
+    trn_data_dir = glob(cfg.ROOT + 'seg_train/*')
+    val_data_dir = glob(cfg.ROOT + 'seg_test/*')
+
+    trn_dataset = datasets.ImageFolder(root=trn_data_dir, transform=trn_transforms)
+    val_dataset = datasets.ImageFolder(root=val_data_dir, transform=val_transforms)
+
+    trn_loader = DataLoader(dataset=trn_dataset, batch_size=cfg.BATCH_SIZE, shuffle=True, num_workers=cfg.NUM_WORKERS)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=cfg.BATCH_SIZE, shuffle=False, num_workers=cfg.NUM_WORKERS)
+
+    return trn_loader, val_loader

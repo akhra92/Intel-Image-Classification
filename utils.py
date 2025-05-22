bd0f57726326266f09f67c119aa43244f7db9148
akhra92 <akhrorrakhmonov@gmail.com>
2025-05-22 16:04:05 +0900

    Update utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import random
+from torchvision.transforms import functional
+
+
+def plot(num_images, dataset, cols=4):
+    rows = (num_images + cols - 1) // cols
+    plt.figure(figsize=(cols*3, rows*3))
+    indices = random.sample(range(len(dataset)), num_images)
+    for i, idx in enumerate(indices):
+        x, y = dataset[idx]
+        plt.subplot(rows, cols, i+1)
+        x = functional.to_pil_image(x)
+        plt.imshow(x)
+        plt.title(dataset.classes[y])
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()

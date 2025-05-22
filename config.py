31f46c265fb6ba1ffac39b3cffe77d43ec36291f
akhra92 <akhrorrakhmonov@gmail.com>
2025-05-22 16:23:00 +0900

    Update config.py file

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import torch
+
+
 ROOT = './dataset/Intel/'
 BATCH_SIZE = 32
 LR = 1e-3
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
 NUM_WORKERS = 2
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'

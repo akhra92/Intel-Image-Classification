9a2d1b312216bce932f70208115b742f83765d2d
akhra92 <akhrorrakhmonov@gmail.com>
2025-05-22 17:09:11 +0900

    Update main.py file

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ trn_loader, val_loader = get_loaders()
 model = timm.create_model(model_name='resnext150', pretrained=True, num_classes=6).to(cfg.DEVICE)
 criterion = nn.CrossEntropyLoss()
 optimizer = AdamW(model.params(), lr=cfg.LR, weight_decay=5e-3)
+best_val_acc = 0.0
+best_val_acc_es = 0.0
+patience = 3
+
 
 def main():
     trn_loss, val_loss = [], []
@@ -29,4 +33,40 @@ def main():
         for batch in tqdm(trn_loader, desc='Training'):
             x, y = x.to(cfg.DEVICE), y.to(cfg.DEVICE)
             acc, loss = train(x, y, model, criterion, optimizer)
-            
+            trn_epoch_acc.append(acc)
+            trn_epoch_loss.append(loss)
+        for batch in tqdm(val_loader, desc='Validation'):
+            x, y = batch
+            x, y = x.to(cfg.DEVICE), y.to(cfg.DEVICE)
+            acc, loss = test(x, y, model, criterion)
+            val_epoch_acc.append(acc)
+            val_epoch_loss.append(loss)
+        
+        trn_epoch_acc = np.mean(trn_epoch_acc)
+        trn_epoch_loss = np.mean(trn_epoch_loss)
+        val_epoch_acc = np.mean(val_epoch_acc)
+        val_epoch_loss = np.mean(val_epoch_loss)
+
+        if val_epoch_acc > best_val_acc:
+            best_val_acc = val_epoch_acc
+            print(f'Saving the model at epoch {epoch+1} with val accuracy {best_val_acc}')
+            torch.save(model.state_dict(), 'best_model.pth')
+        
+        if val_epoch_acc > best_val_acc_es:
+            best_val_acc_es = val_epoch_acc
+            count = 0
+        else:
+            count += 1
+            print(f'No improvement for {count} times.')
+            if count > patience:
+                break
+        
+        trn_acc.append(trn_epoch_acc)
+        trn_loss.append(trn_epoch_loss)
+        val_acc.append(val_epoch_acc)
+        val_loss.append(val_epoch_loss)
+
+
+if __name__ == '__main__':
+    plot(20)
+    main()

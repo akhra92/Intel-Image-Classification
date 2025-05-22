02d3c304d029a313fe72aca99341c7b0c6d9c4e0
akhra92 <akhrorrakhmonov@gmail.com>
2025-05-22 16:18:37 +0900

    Update train.py file

--- train.py
+++ train.py
@@ -4,3 +4,17 @@ import torch
 import torchvision
 import numpy as np
 
+
+def train(x, y, model, criterion, optimizer):
+    model.train()
+    pred = model(x)
+    loss = criterion(pred, y)
+    
+    predicted = torch.argmax(pred, dim=1)
+    acc = (predicted == y).float().mean()
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss.item(), acc.item()

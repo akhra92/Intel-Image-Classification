7ee36922d504c07bf9c702f46c2f70115cd94117
akhra92 <akhrorrakhmonov@gmail.com>
2025-06-07 16:20:00 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -12,7 +12,7 @@ def run(args):
     cls_names = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
     num_classes = len(cls_names)
     tfs = get_transforms(train=False)
-    default_path = 'sample_images/465.jpg'
+    default_path = 'sample_images/475.jpg'
     sample_path = 'assets/samples.png'
 
     model = load_model(num_classes, args.checkpoint_path)
